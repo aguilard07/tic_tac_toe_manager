80b79370b47df91ac1b72ba32f1131e0bbdbf6de
= <=>
2021-01-25 05:45:06 -0400

    Tictactoe cli game implemented

--- example/tic_tac_toe_manager_example.dart
+++ example/tic_tac_toe_manager_example.dart
@@ -1,18 +1,44 @@
-import 'package:tic_tac_toe_manager/tic_tac_toe_manager.dart';
+import 'dart:io';
+
+import '../lib/src/tic_tac_toe_manager_base.dart';
 
 void main() {
   // ignore: omit_local_variable_types
   TicTacToeManager ticTacToeManager = TicTacToeManager();
+  bool isTied = false;
+  bool isWon = false;
+  String currentPlayer;
+  Map<String, dynamic> gameData;
 
-  /*print(ticTacToeManager.getBoardState);*/
+  gameData = ticTacToeManager.startGame(false);
 
-  ticTacToeManager.update('X', 0);
-  ticTacToeManager.update('O', 4);
-  ticTacToeManager.update('X', 1);
-  ticTacToeManager.update('O', 8);
-  ticTacToeManager.update('X', 2);
-  /* ticTacToeManager.update('O', 2);
-  ticTacToeManager.update('X', 5);
-  ticTacToeManager.update('O', 3);
-  ticTacToeManager.update('X', 7);*/
+  while (!isWon && !isTied) {
+    currentPlayer = gameData['nextPlayer'];
+    stdout.writeln('The current player is: ${currentPlayer}');
+    stdout.writeln('Current state of the board:');
+    ticTacToeManager.printBoardState();
+    stdout.writeln('Please introduce the index where you want to play:');
+
+    //Reading the user input (next play).
+    try {
+      int index = int.parse(stdin
+          .readLineSync()!); //Reads the user input and validates that is not null and an int value.
+      gameData = ticTacToeManager.update(currentPlayer, index, false);
+
+      print(gameData);
+
+      isWon = gameData['isWon'];
+      isTied = gameData['isTied'];
+
+      if (isWon) {
+        stdout.writeln('The winner player is ${gameData['winnerPlayer']}');
+        stdout.writeln('The winner play is: ${gameData['winnerLine']}');
+      } else if (isTied) {
+        stdout.writeln('The game is tied.');
+      }
+    } catch (_) {
+      stdout
+          .writeln('Please insert a correct value. Must be an int between 0-8');
+    }
+  }
 }
--- lib/src/tic_tac_toe_manager_base.dart
+++ lib/src/tic_tac_toe_manager_base.dart
@@ -1,35 +1,31 @@
-import 'package:tic_tac_toe_manager/src/tic_tac_toe_utils.dart';
+import 'tic_tac_toe_utils.dart';
 
 ///Manager that handles the procedures related to a Tic Tac Toe Match.
 ///It handles the turns, calculation of winners and ties and updates the
 ///state of the board.
 
-//TODO: Logic of tie.
-//TODO: validatePlayer method.
-
 class TicTacToeManager {
-  Map<int, String> _boardState; //Board's current state.
-
-  bool _isTied; //Tied state
-  bool _isWon;
-  bool _isLastPlayValid; //Checks if the last play is valid.
-  String _winnerPlayer; //Winner state
-  String _nextPlayer;
-
-  List<int> _winnerLine;
-
-  TicTacToeManager() {
-    _boardState = <int, String>{};
-    _isTied = false;
-  }
+  Map<int, String> _boardState = <int, String>{}; //Board's current state.
+  bool _isTied = false; //Tied state
+  bool _isWon = false;
+  bool _isLastPlayValid = false; //Checks if the last play is valid.
+  String _winnerPlayer = ''; //Winner state
+  String _nextPlayer = 'X';
+  List<int> _winnerLine = [];
 
   Map get getBoardState => _boardState;
 
   void printBoardState() {
-    print(_boardState);
+    print(
+        '|${_boardState[0] ?? 'A'}     ${_boardState[1] ?? 'A'}     ${_boardState[2] ?? 'A'}|');
+    print(
+        '|${_boardState[3] ?? 'A'}     ${_boardState[4] ?? 'A'}     ${_boardState[5] ?? 'A'}|');
+    print(
+        '|${_boardState[6] ?? 'A'}     ${_boardState[7] ?? 'A'}     ${_boardState[8] ?? 'A'}|');
   }
 
-  void update(String currentPlayer, int index) {
+  Map<String, dynamic> update(
+      String currentPlayer, int index, bool printBoard) {
     _isLastPlayValid = false;
 
     // 1. Chequear número de jugadas.
@@ -38,22 +34,18 @@ class TicTacToeManager {
       _isLastPlayValid = _updateBoard(currentPlayer, index);
       // 3. Determine if the is a winner, taking into account the last play.
 
-      //It required at least 5 plays to have a winner.
+      //Note: It's required at least 5 plays to have a winner.
       if (_isLastPlayValid) {
         if (_boardState.length >= 5) {
           _calculateWinner(currentPlayer);
-
           //4.If 8 plays has been made and there's no winner, so the game is tied.
-          if (!_isWon && _boardState.length == 8) _isTied = true;
+          if (!_isWon && _boardState.length == 9) _isTied = true;
         }
         //5. Determining next player turn.
         _determineNextPlayer(currentPlayer);
       }
     }
-
-    //TODO: retornar json
-
-    //Si no hay ganador, ni empate, devuelve el siguiente jugador.
+    return gameDataToJson();
   }
 
   void _calculateWinner(String currentPlayer) {
@@ -65,12 +57,9 @@ class TicTacToeManager {
       if (value == currentPlayer) playerMarks.add(key);
     });
 
-    print('El jugador es $currentPlayer');
-    print('Jugadas: $playerMarks');
-
     //2. Calcular si hay ganador.
 
-    for (List line in winnerLists) {
+    for (List<int> line in winnerLists) {
       _isWon = line.every((element) => playerMarks.contains(element));
       //Si hay ganador.
       if (_isWon) {
@@ -78,48 +67,49 @@ class TicTacToeManager {
         _winnerLine = line;
         break;
       }
-
-      print('Winner player: $_winnerPlayer');
-      print('Winner line: $_winnerLine');
     }
   }
 
   /*
-    La jugada está representada por un index y una marca (Mark)
-    Actualiza la marca del index solamente si previamente no se ha realizado una
-    jugada en ese index.
-  
-    Retorna true si la jugada es válida, false caso contrario
+    Each play is represented by two values a player mark ('X' || 'O')
+    and an index.
+    The play is valid only if the mark is X or O and the index is positive and
+    between the range [0,8]
   */
   bool _updateBoard(String player, int index) {
-    if (_boardState.containsKey(index)) {
+    if (player == _nextPlayer && index.abs() < 9) {
+      if (_boardState.containsKey(index)) {
+        return false;
+      }
+      _boardState.putIfAbsent(index, () => player);
+      return true;
+    } else {
       return false;
     }
-
-    _boardState.putIfAbsent(index, () => player);
-    return true;
   }
 
   void _determineNextPlayer(String currentPlayer) {
-    _nextPlayer = currentPlayer == 'X' ? 'O' : 'X';
+    currentPlayer == 'X' ? _nextPlayer = 'O' : _nextPlayer = 'X';
   }
 
-  void restartGame() {
+  Map<String, dynamic> startGame(bool printBoard) {
     _nextPlayer = 'X';
     _isWon = false;
     _isTied = false;
-    _winnerLine = null;
-    _winnerPlayer = null;
+    _winnerLine = [];
+    _winnerPlayer = '';
     _boardState.clear();
+    if (printBoard) printBoardState();
+    return gameDataToJson();
   }
 
   /*
       {
         "isLastPlayValid": true,
-        "nextPlayer": Mark.PlayerO,
+        "nextPlayer": 'X',
         "isTied": false,
         "isWon": false,
-        "winnerPlayer": PlayerX,
+        "winnerPlayer": 'O',
         "winnlerLine": [0, 1 ,2]
       }
     
