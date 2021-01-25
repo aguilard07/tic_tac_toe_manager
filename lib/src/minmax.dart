79adb2108699a27d5301e287653f027c96a8dc68
Daniel Aguilar <daguilopez07@gmail.com>
2021-01-25 16:15:31 -0400

    Agregada funcion de fitness

---
+++ lib/src/minmax.dart
@@ -0,0 +1,27 @@
+class Minmax {
+  int calculateFitness(Map<int, String> boardState, String currentPlayer,
+      List<List<int>> winnerLines) {
+    var playerMarks = [];
+    var score = 0;
+
+    boardState.forEach((key, value) {
+      if (value == currentPlayer) playerMarks.add(key);
+    });
+
+    for (var line in winnerLines) {
+      // ignore: omit_local_variable_types
+      bool isWon = line.every((element) => playerMarks.contains(element));
+      //Si hay ganador.
+      if (isWon) {
+        if (currentPlayer == 'X') {
+          score = 10;
+        } else {
+          score = -10;
+        }
+        break;
+      }
+    }
+
+    return score;
+  }
+}
